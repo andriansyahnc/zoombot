fbeb311c166f36c2fde1fdca2ff490adfb467a91
Alfan <ahmad.alpan@gmail.com>
2019-10-18 06:46:48 +0700

    Update Index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
 const axios = require('axios')
 
 app.use(bodyParser.json())

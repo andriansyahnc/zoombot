7f90d9d060e8c4a6cbadbcb35756e26469aaf9b3
Alfan <ahmad.alpan@gmail.com>
2019-10-18 06:51:54 +0700

    Port

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/new-message', function(req, res) {
 })
 
 // Finally, start our server
-app.listen(3000, function() {
+app.listen(8080, function() {
   console.log('Telegram app listening on port 3000!')
 })
 

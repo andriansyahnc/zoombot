1b2fa38e8b0f5e6559add3e3d324b85c1dd2edb0
Alfan <ahmad.alpan@gmail.com>
2019-10-18 05:58:32 +0700

    Update

--- index.js
+++ index.js
@@ -3,43 +3,41 @@ const app = express()
 const bodyParser = require('body-parser')
 const axios = require('axios')
 
-app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
-) // for parsing application/x-www-form-urlencoded
+)
 
 //This is the route the API will call
 app.post('/new-message', function(req, res) {
   const { message } = req.body
   console.log('message', message)
 
-  //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
-
-  if (!message || (message.text.toLowerCase().indexOf('meeting') < 0 && message.text.toLowerCase().indexOf('sis') < 0)) {
-    // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
+  if (!message) {
     return res.end()
   }
 
-  // If we've gotten this far, it means that we have received a message containing the word "marco".
-  // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
-  // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
+  const responseMessage = defineMessage(message)
+
+  if (!responseMessage) {
+    return res.end()
+  }
+
   axios
     .post(
       'https://api.telegram.org/bot910597252:AAEfDXVyBpI2Hwoh093uBAP-XJgOAW8d2Vg/sendMessage',
       {
         chat_id: message.chat.id,
-        text: 'iya, nih aku buatin linknya. https://zoom.us/j/9046286650'
+        text: responseMessage
       }
     )
     .then(response => {
-      // We get here if the message was successfully posted
       console.log('Message posted')
       res.end('ok')
     })
     .catch(err => {
-      // ...and here if it was not
       console.log('Error :', err)
       res.end('Error :' + err)
     })
@@ -49,3 +47,20 @@ app.post('/new-message', function(req, res) {
 app.listen(3000, function() {
   console.log('Telegram app listening on port 3000!')
 })
+
+defineMessage = (message) => {
+    let result = false
+    switch(true) {
+        case (message.text.toLowerCase().indexOf('meeting sis') >= 0):
+            result = 'iya, nih aku buatin linknya. https://zoom.us/j/9046286650'
+            break
+        case (message.text.toLowerCase().indexOf('mabar sis') >= 0):
+            result = 'kuy, ditunggu di loby. @shinichi_coding, @rizkydh, @trastanechora, @wibymf, @kobarseptyanus, @angga_dar, @azulkipli'
+            break
+        default:
+            result = false
+            break
+    }
+
+    return result
+}

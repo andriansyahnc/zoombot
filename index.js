a190cb14e4d6f4ffa541e8995b51ab7749ba8fea
Alfan <ahmad.alpan@gmail.com>
2019-10-18 08:40:57 +0700

    Change wording of zoom link

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.listen(3000, function() {
 // Define the result message
 defineMessage = (message) => {
     if (!message || (message && typeof message.text === 'undefined')) {
-        return false
+      return false
     }
     
     let result = false
@@ -63,7 +63,7 @@ defineMessage = (message) => {
           message.text.toLowerCase().indexOf('zoom kuy') >= 0 ||
           message.text.toLowerCase().indexOf('meeting sis') >= 0
         )):
-          result = 'iya, nih aku buatin linknya. https://zoom.us/j/9046286650'
+          result = 'kuy kak, pake link ini ya.. https://zoom.us/j/9046286650'
           break
       case (
         message.text.toLowerCase().indexOf('mabar') >= 0 &&
@@ -74,23 +74,23 @@ defineMessage = (message) => {
           result = 'kuy, ditunggu di loby nih. \n@shinichi_coding, @rizkydh, @trastanechora, @wibymf, @kobarseptyanus, @angga_dar, @king_of_bros, @tofas24, @azulkipli'
           break
       case (message.text.toLowerCase().indexOf('sabar sis') >= 0):
-          result = 'iya kak, aku orangnya penyabar kok :\')'
-          break
+        result = 'iya kak, aku orangnya penyabar kok :\')'
+        break
       case (message.text.toLowerCase().indexOf('kobar sis') >= 0):
-          result = 'kak @kobarseptyanus dipanggil tuh. mau diajak maen.'
-          break
+        result = 'kak @kobarseptyanus dipanggil tuh. mau diajak maen.'
+        break
       case (message.text.toLowerCase().indexOf('bubar sis') >= 0):
-          result = 'jahadd. aku disuruh bubar :\'('
-          break
+        result = 'jahadd. aku disuruh bubar :\'('
+        break
       case (message.text.toLowerCase().indexOf('ada yang lagi test?') >= 0):
-          result = 'tuh ditanyain lho. ada yang lagi test gak nih? kita mau merge lho :( \n@zhafiranisa, @safikaa, @shelayunita, @herawati_m, @dininovarianti, @fajartabuti, @Ferdi182'
-          break
+        result = 'tuh ditanyain lho. ada yang lagi test gak nih? kita mau merge lho :( \n@zhafiranisa, @safikaa, @shelayunita, @herawati_m, @dininovarianti, @fajartabuti, @Ferdi182'
+        break
       case (message.text.toLowerCase().indexOf('makasih sis') >= 0):
-          result = 'sama-sama kakak.. :)'
-          break
+        result = 'sama-sama kakak.. :)'
+        break
       default:
-          result = false
-          break
+        result = false
+        break
     }
 
     return result

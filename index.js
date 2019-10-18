eb25f8fe4be5ba7d8edb86d977cfc08d200153fd
Alfan <ahmad.alpan@gmail.com>
2019-10-18 08:27:54 +0700

    Add New Command

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(
 //This is the route the API will call
 app.post('/new-message', function(req, res) {
   const { message } = req.body
-  console.log('message', message)
+  // console.log('message', message)
 
   if (!message) {
     return res.end()
@@ -55,21 +55,33 @@ defineMessage = (message) => {
     
     let result = false
     switch(true) {
-        case ((message.from.first_name.toLowerCase().indexOf('aryo') >= 0 || message.from.first_name.toLowerCase().indexOf('alfan') >= 0 || message.from.first_name.toLowerCase().indexOf('satria') >= 0 || message.from.first_name.toLowerCase().indexOf('imam') >= 0) && message.text.toLowerCase().indexOf('meeting sis') >= 0):
-            result = 'iya, nih aku buatin linknya. https://zoom.us/j/9046286650'
-            break
-        case (message.text.toLowerCase().indexOf('mabar sis') >= 0):
-            result = 'kuy, ditunggu di loby. @shinichi_coding, @rizkydh, @trastanechora, @wibymf, @kobarseptyanus, @angga_dar, @azulkipli'
-            break
-        case (message.text.toLowerCase().indexOf('ada yang lagi test?') >= 0):
-            result = 'tuh ditanyain lho. ada yang lagi test gak nih? mau merge lho :('
-            break
-        case (message.from.first_name.toLowerCase().indexOf('alfan') >= 0 && message.text.toLowerCase().indexOf('makasih sis') >= 0):
-            result = 'sama-sama tampan :)'
-            break
-        default:
-            result = false
-            break
+      case (
+        (
+            message.from.first_name.toLowerCase().indexOf('aryo') >= 0 ||
+            message.from.first_name.toLowerCase().indexOf('satria') >= 0 ||
+            message.from.first_name.toLowerCase().indexOf('imam') >= 0 ||
+            message.from.first_name.toLowerCase().indexOf('alfan') >= 0
+        ) && message.text.toLowerCase().indexOf('meeting sis') >= 0):
+          result = 'iya, nih aku buatin linknya. https://zoom.us/j/9046286650'
+          break
+      case (message.text.toLowerCase().indexOf('mabar sis') >= 0):
+          result = 'kuy, ditunggu di loby. @shinichi_coding, @rizkydh, @trastanechora, @wibymf, @kobarseptyanus, @angga_dar, @king_of_bros, @tofas24, @azulkipli'
+          break
+      case (message.text.toLowerCase().indexOf('sabar sis') >= 0):
+          result = 'iya kak, aku orangnya penyabar kok :\')'
+          break
+      case (message.text.toLowerCase().indexOf('kobar sis') >= 0):
+          result = 'kak @kobarseptyanus dipanggil tuh. mau diajak maen.'
+          break
+      case (message.text.toLowerCase().indexOf('ada yang lagi test?') >= 0):
+          result = 'tuh ditanyain lho. ada yang lagi test gak nih? mau merge lho :( \n @zhafiranisa, @safikaa, @shelayunita, @herawati_m, @dininovarianti, @fajartabuti, @Ferdi182'
+          break
+      case (message.text.toLowerCase().indexOf('makasih sis') >= 0):
+          result = 'sama-sama tampan :)'
+          break
+      default:
+          result = false
+          break
     }
 
     return result
